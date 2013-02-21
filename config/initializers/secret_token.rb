1fb661dd925e05cffe26cab6d67cad797ac3d04b
maisaengineering <contact@maisasolutions.com>
2013-02-21 12:27:33 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ladderboost::Application.config.secret_token = '56036bb5fc7213b94699f4d1d2872629abd11d539ea9b26ce4ded8dde0d6f7f1215f35371ef9cbcc65b09212357c4d1528d5bffaffd50968ce8d254c1abb4e74'
