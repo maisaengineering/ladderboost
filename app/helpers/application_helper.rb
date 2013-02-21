d0552427c83932349d0366741cbb8420d5e29aac
maisaengineering <contact@maisasolutions.com>
2013-02-21 15:28:19 +0530

    nav links active class added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  #Navigation Link if current_page is true add the class 'active'
+  # link_to 'some',path,class: "#{active_class?(path)} additional_class"
+  def active_class?(path)
+    'active' if current_page?(path)
+  end
+
 end
