1e0fe32d09ba215b6173ae769406c1f7cc37966d
maisaengineering <contact@maisasolutions.com>
2013-02-26 15:02:28 +0530

    configured confirmation mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,18 +13,30 @@ Ladderboost::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    #domain: "example.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
@@ -32,17 +44,14 @@ Ladderboost::Application.configure do
   }
 
 
+  #config.action_mailer.smtp_settings = {
+  #    :enable_starttls_auto => true,
+  #    :address => "smtp.gmail.com",
+  #    :port => 587,
+  #    :domain => "gmail.com",
+  #    :authentication => :login,
+  #    :user_name => ENV["GMAIL_USERNAME"],
+  #    :password => ENV["GMAIL_PASSWORD"],
+  #}
 
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
-
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
-
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
 end
