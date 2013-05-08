2fd74a3b0561414cf5413d040ace02c65d8cf823
maisa engineering <contact@maisasolutions.com>
2013-05-08 17:37:17 +0530

    added signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout_by_resource
+
+  protected
+ # Below method is used not to render layout for sign up pages
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+ # Below method is used not to render layout for sign up pages
+
  # add_breadcrumb :index, :root_path
 
   #If the user authorization fails, a CanCan::AccessDenied exception will be raised. You can catch this like
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Ladderboost::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users ,controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+
   devise_scope :user do
     # match "/login" => "devise/sessions#new"
     # delete "/logout" => "devise/sessions#destroy"
