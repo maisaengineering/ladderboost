a97fcd99c5ec4b48a75a3ba95a33a479b0f6eac2
maisa engineering <contact@maisasolutions.com>
2013-05-18 17:35:59 +0530

    praneeth: changed my_account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Ladderboost::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
@@ -39,8 +39,8 @@ Ladderboost::Application.configure do
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "addament.engineer@gmail.com",
+    password: "addament123#"
   }
 
 
